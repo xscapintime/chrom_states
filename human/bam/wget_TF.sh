b379ff4bb4b67193b7df59fb09a05e855ee2f007
wkat.biocluster <louiesxscape@gmail.com>
2021-12-03 22:10:01 +0800

    ChromHMM bin and learn

--- human/bam/wget_TF.sh
+++ human/bam/wget_TF.sh
@@ -1,2 +1,2 @@
-wget -c https://www.encodeproject.org/files/ENCFF616PLK/@@download/ENCFF616PLK.bam -O CTCF_ENCFF616PLK.bam
-wget -c https://www.encodeproject.org/files/ENCFF661SFY/@@download/ENCFF661SFY.bam -O POLR2A_ENCFF661SFY.bam
+wget -c https://www.encodeproject.org/files/ENCFF616PLK/@@download/ENCFF616PLK.bam -O CTCF-human.ENCFF616PLK.bam
+wget -c https://www.encodeproject.org/files/ENCFF661SFY/@@download/ENCFF661SFY.bam -O POLR2A-human.ENCFF661SFY.bam
